9214b9331907df51c30c6cb36c4436c7e531087f
Joshua J. Kane <kanejj@inl600228.inl.gov>
2015-02-26 16:29:46 -0700

    Beginning my new application

---
+++ include/base/GopherApp.h
@@ -0,0 +1,22 @@
+#ifndef GOPHERAPP_H
+#define GOPHERAPP_H
+
+#include "MooseApp.h"
+
+class GopherApp;
+
+template<>
+InputParameters validParams<GopherApp>();
+
+class GopherApp : public MooseApp
+{
+public:
+  GopherApp(const std::string & name, InputParameters parameters);
+  virtual ~GopherApp();
+
+  static void registerApps();
+  static void registerObjects(Factory & factory);
+  static void associateSyntax(Syntax & syntax, ActionFactory & action_factory);
+};
+
+#endif /* GOPHERAPP_H */
--- src/base/GopherApp.C
+++ src/base/GopherApp.C
@@ -1,10 +1,10 @@
-#include "StorkApp.h"
+#include "GopherApp.h"
 #include "Moose.h"
 #include "AppFactory.h"
 #include "ModulesApp.h"
 
 template<>
-InputParameters validParams<StorkApp>()
+InputParameters validParams<GopherApp>()
 {
   InputParameters params = validParams<MooseApp>();
 
@@ -13,37 +13,37 @@ InputParameters validParams<StorkApp>()
   return params;
 }
 
-StorkApp::StorkApp(const std::string & name, InputParameters parameters) :
+GopherApp::GopherApp(const std::string & name, InputParameters parameters) :
     MooseApp(name, parameters)
 {
   srand(processor_id());
 
   Moose::registerObjects(_factory);
   ModulesApp::registerObjects(_factory);
-  StorkApp::registerObjects(_factory);
+  GopherApp::registerObjects(_factory);
 
   Moose::associateSyntax(_syntax, _action_factory);
   ModulesApp::associateSyntax(_syntax, _action_factory);
-  StorkApp::associateSyntax(_syntax, _action_factory);
+  GopherApp::associateSyntax(_syntax, _action_factory);
 }
 
-StorkApp::~StorkApp()
+GopherApp::~GopherApp()
 {
 }
 
-extern "C" void StorkApp__registerApps() { StorkApp::registerApps(); }
+extern "C" void GopherApp__registerApps() { GopherApp::registerApps(); }
 void
-StorkApp::registerApps()
+GopherApp::registerApps()
 {
-  registerApp(StorkApp);
+  registerApp(GopherApp);
 }
 
 void
-StorkApp::registerObjects(Factory & factory)
+GopherApp::registerObjects(Factory & factory)
 {
 }
 
 void
-StorkApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
+GopherApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
 {
 }
